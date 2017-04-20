2d224b3fa1a58e0113fff520aecf8d54b2cb6aa9
Shubham Jha <startupsansar@gmail.com>
2017-04-20 09:41:38 +0530

    updated db for demo version

--- database/test.sql
+++ database/test.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 18, 2017 at 03:45 PM
+-- Generation Time: Apr 20, 2017 at 06:10 AM
 -- Server version: 10.1.19-MariaDB
 -- PHP Version: 7.0.13
 
@@ -32,21 +32,25 @@ CREATE TABLE `classes` (
   `location` varchar(50) NOT NULL,
   `email` varchar(100) NOT NULL,
   `phone` decimal(10,0) UNSIGNED NOT NULL,
-  `description` longtext NOT NULL
+  `logourl` text,
+  `pageurl` text,
+  `description` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `classes`
 --
 
-INSERT INTO `classes` (`classid`, `cname`, `location`, `email`, `phone`, `description`) VALUES
-(201, 'Vidyalankar', 'Andheri', 'vidyalankar@class.com', '111111111', 'this is the best classes ever'),
-(202, 'RaoIIT', 'Borivali', 'raoiit@class.com', '2222222222', 'I will declare this classes as no.2'),
-(203, 'Badhe', 'Vasai', 'badhe@class.com', '3333333333', 'any random shit here'),
-(204, 'PaceIIT', 'Bandra', 'paceiit@class.com', '9999999999', 'How come we have not mentioned the money this classes earns'),
-(205, 'Vidyasagar', 'Dadar', 'vidyasagar@class.com', '9100000000', 'never heard of this one yet, maybe founders are vidya and sagar'),
-(206, 'Bhavani-Classes', 'Nalasopara', 'bhavaniclasses@gmailcom', '9122336474', 'jai bhavani'),
-(207, 'Allen', 'Bhayandar', 'alleninstitute@gmailcom', '7506716217', 'We-are-Allen-the-best-IIT-institute-out-their');
+INSERT INTO `classes` (`classid`, `cname`, `location`, `email`, `phone`, `logourl`, `pageurl`, `description`) VALUES
+(208, 'Rao-IIT-Academy', 'Andheri', 'contactus@raoiit.com', '3245345345', 'raoiit.png', 'post-raoiit.html', 'Rao-IIT-does-not-belong-to-Siddesh-Rao'),
+(209, 'Resonance', 'Borivali', 'contactus@resonance.com', '1234123498', 'resonance.png', 'post-resonance.html', 'Resonance-is-the-IIT-coaching-ever'),
+(211, 'Ace-Coaching', 'Nalasopara', 'contactus@ace.com', '1212121211', 'ace.png', 'post-ace.html', 'I-dont-where-is-this'),
+(212, 'CET-King', 'Virar', 'contactus@cetking.com', '1321223232', 'cetking.png', 'post-cetking.html', 'We-need-a-entrance-exam-section-for-each-of-the-streams'),
+(213, 'The-Gate-Academy', 'Bandra', 'contactus@gateacademy.com', '7737377373', 'gateacademy.png', 'post-gateacademy.html', 'the-gate-academy-provides-gate-preparation-for-IT-and-Mechanical'),
+(214, 'Progate-Coaching', 'Dadar', 'contactus@progatecoaching.com', '7362361717', 'progate.png', 'post-progate.html', 'progate-coaching-provides-gate-preparation-for-computer-and-civil'),
+(215, 'Ideal-st-Century', 'Mira-Road', 'contactus@idealcentury.com', '7432892428', 'ideal.png', 'post-ideal.html', 'this-one-offers-subjects-Accountancy-and-Book-Keeping-in-BFM'),
+(216, 'Lokayan', 'Dahisar', 'contactus@lokayan.com', '3242434354', 'lokayan.png', 'post-lokayan.html', 'Lokayan-Provides-Preparation-for-Government-Exams'),
+(217, 'Focus-Competition', 'Goregaon', 'contactus@focuscompetition.com', '6726262676', 'focuscompetition.png', 'post-focuscompetition.html', 'Focus-Competition-provides-preparation-for-Entrance-Exams-such-as-SBI-PO-and-Civil-Services');
 
 -- --------------------------------------------------------
 
@@ -84,7 +88,7 @@ INSERT INTO `locations` (`loc`) VALUES
 ('Dahisar'),
 ('Goregaon'),
 ('Mira Road'),
-('Nallasopara'),
+('Nalasopara'),
 ('Vasai'),
 ('Virar');
 
@@ -128,28 +132,18 @@ CREATE TABLE `map_class_stream` (
 --
 
 INSERT INTO `map_class_stream` (`class_id`, `stream_id`) VALUES
-(201, 701),
-(201, 702),
-(201, 703),
-(201, 704),
-(201, 705),
-(201, 706),
-(202, 707),
-(203, 701),
-(203, 702),
-(203, 703),
-(203, 704),
-(203, 705),
-(203, 706),
-(204, 707),
-(205, 701),
-(205, 702),
-(205, 703),
-(205, 704),
-(205, 705),
-(205, 706),
-(206, 713),
-(207, 707);
+(208, 707),
+(209, 707),
+(211, 701),
+(211, 702),
+(212, 707),
+(213, 701),
+(213, 703),
+(214, 702),
+(214, 704),
+(215, 710),
+(216, 728),
+(217, 728);
 
 -- --------------------------------------------------------
 
@@ -167,17 +161,31 @@ CREATE TABLE `map_class_subject` (
 --
 
 INSERT INTO `map_class_subject` (`class_id`, `subject_id`) VALUES
-(201, 101),
-(201, 102),
-(201, 104),
-(202, 106),
-(203, 102),
-(203, 105),
-(204, 106),
-(205, 101),
-(205, 103),
-(206, 112),
-(207, 106);
+(208, 106),
+(208, 113),
+(208, 114),
+(209, 106),
+(209, 113),
+(209, 114),
+(211, 102),
+(211, 105),
+(211, 108),
+(211, 110),
+(212, 106),
+(212, 113),
+(212, 114),
+(213, 103),
+(213, 108),
+(213, 109),
+(214, 101),
+(214, 110),
+(215, 115),
+(215, 116),
+(216, 117),
+(216, 118),
+(216, 119),
+(217, 117),
+(217, 119);
 
 -- --------------------------------------------------------
 
@@ -208,7 +216,14 @@ INSERT INTO `subject` (`subjectid`, `sname`, `semester`, `university`, `fstream`
 (109, 'Big-data-analysis', 'semester ', 'Mumbai-University', 'Information-Technology'),
 (110, 'Web-Development', 'semester ', 'Mumbai-University', 'Computer'),
 (111, 'Jewelry-Design', 'semester ', 'Raffels', 'CAD-Design'),
-(112, 'Cosmetics', 'semester-', 'mumbai-university', 'Fashion-Designing');
+(112, 'Cosmetics', 'semester', 'mumbai-university', 'Fashion-Designing'),
+(113, 'Chemistry', 'hsc', 'IITJEE', 'IIT'),
+(114, 'Mathematics', 'hsc', 'IITJEE', 'IIT'),
+(115, 'Accountancy', 'Semester-', 'Pune-University', 'BFM'),
+(116, 'Book-Keeping', 'semester-', 'Pune-University', 'BFM'),
+(117, 'SBI-PO', 'GRADUATE', 'SBI', 'Entrance'),
+(118, 'Staff-Selection-Commission', 'GRADUATE', 'Government', 'Entrance'),
+(119, 'Civil-Services', 'GRADUATE', 'Government', 'Entrance');
 
 -- --------------------------------------------------------
 
@@ -240,13 +255,13 @@ INSERT INTO `substream` (`ssid`, `ssname`, `msname`) VALUES
 (712, 'LLB', 'Law'),
 (713, 'Fashion-Designing', 'Arts'),
 (715, 'Aeronautical', 'Engineering'),
-(718, 'Chemistry', 'Science'),
 (719, 'Literature', 'Arts'),
 (722, 'BMS', 'Medical'),
 (724, 'Deitician', 'Medical'),
 (725, 'CA', 'Commerce'),
 (726, 'CAD-Design', 'Arts'),
-(727, 'Communication-Design', 'Design');
+(727, 'Communication-Design', 'Design'),
+(728, 'Entrance', 'Engineering');
 
 -- --------------------------------------------------------
 
@@ -394,7 +409,7 @@ ALTER TABLE `tags`
 -- AUTO_INCREMENT for table `classes`
 --
 ALTER TABLE `classes`
-  MODIFY `classid` int(15) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=208;
+  MODIFY `classid` int(15) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=218;
 --
 -- AUTO_INCREMENT for table `mainstream`
 --
@@ -404,12 +419,12 @@ ALTER TABLE `mainstream`
 -- AUTO_INCREMENT for table `subject`
 --
 ALTER TABLE `subject`
-  MODIFY `subjectid` int(15) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=113;
+  MODIFY `subjectid` int(15) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=120;
 --
 -- AUTO_INCREMENT for table `substream`
 --
 ALTER TABLE `substream`
-  MODIFY `ssid` int(15) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=728;
+  MODIFY `ssid` int(15) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=729;
 --
 -- Constraints for dumped tables
 --
