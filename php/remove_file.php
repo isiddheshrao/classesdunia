647e2baade6389e5908958740e3381f3392d820a
Shubham Jha <startupsansar@gmail.com>
2017-05-28 23:42:30 +0530

    changes to make file upload work

--- php/remove_file.php
+++ php/remove_file.php
@@ -1,6 +1,7 @@
 <?php
 if(isset($_POST['file'])){
-    $file = '../uploads/' . $_POST['file'];
+    $class = $_POST['file'];
+    $file = '../'.$class.'/'.$class.'_logo.png';
     if(file_exists($file)){
         unlink($file);
     }
