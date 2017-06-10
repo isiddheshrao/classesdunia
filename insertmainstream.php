56994c15f3d375094db985186dfc14e849bb728d
Shubham Jha <startupsansar@gmail.com>
2017-06-10 14:12:56 +0530

    for adding mainstream columns to subject table

---
+++ insertmainstream.php
@@ -0,0 +1,23 @@
+<?php
+require 'connect.php';
+
+try {
+    $conn = new PDO("mysql:host=".DB_SERVER.";dbname=".DB_NAME, DB_USER, DB_PASSWORD);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $stmt0 = $conn->prepare('SELECT DISTINCT fstream FROM subject');
+    $stmt0->execute();
+    while($row0 = $stmt0->fetch())
+    {
+      $stmt1 = $conn->prepare('SELECT ssname,msname FROM substream WHERE ssname=:fstream');
+      $stmt1->execute(array('fstream' => $row0['fstream']));
+      if($stmt1->rowCount()>0){
+        $row1 = $stmt1->fetch();
+        $stmt2 = $conn->prepare('UPDATE `subject` SET `fmstream`=:mainstream WHERE `fstream`=:substream');
+        $stmt2->execute(array('mainstream' => $row1['msname'], 'substream' => $row1['ssname']));
+      }
+    }
+  }
+  catch(PDOException $e) {
+      echo 'ERROR: ' . $e->getMessage();
+  }
+?>
